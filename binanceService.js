84622f1b2f56be72cdaa35e090cb30c4f1c8b081
valarxu <7781862@qq.com>
2025-02-23 19:50:48 +0800

    fix: 移除bn代理

--- binanceService.js
+++ binanceService.js
@@ -1,12 +1,5 @@
 const config = require('./config');
 const { axiosInstance, sleep } = require('./utils');
-const axios = require('axios');
-
-// 创建axios实例时移除代理配置
-const client = axios.create({
-    baseURL: 'https://fapi.binance.com',
-    timeout: 10000,
-});
 
 class BinanceService {
     // 获取所有活跃合约信息
