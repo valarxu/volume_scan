73f768f3dfe6e585ffe21cc9efc47a8ea282ba52
valarxu <7781862@qq.com>
2025-02-23 19:45:38 +0800

    fix: 移除utils代理

--- utils.js
+++ utils.js
@@ -1,14 +1,9 @@
 const axios = require('axios');
 const config = require('./config');
-const { HttpsProxyAgent } = require('https-proxy-agent');
-
-// 创建代理agent
-const proxyAgent = new HttpsProxyAgent(`http://${config.PROXY_CONFIG.host}:${config.PROXY_CONFIG.port}`);
 
 // 创建axios实例
 const axiosInstance = axios.create({
     timeout: 10000,
-    httpsAgent: proxyAgent,
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
